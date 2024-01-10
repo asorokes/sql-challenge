761ce090b64783539ac81d1da1ade844093d1515
Adam Sorokes <148814847+asorokes@users.noreply.github.com>
2024-01-09 19:52:37 -0500

    Update employees_sql.sql

--- employees_sql.sql
+++ employees_sql.sql
@@ -2,7 +2,7 @@
 CREATE TABLE titles (
     title_id varchar not null,
     title varchar not null,
-	primary key (title_id)
+    primary key (title_id)
 );
 
 --create employees table
@@ -14,37 +14,37 @@ CREATE TABLE employees(
     last_name varchar not null,
     sex varchar not null,
     hire_date date not null,
-	primary key (emp_no),
-	foreign key (emp_title_id) references titles (title_id)
+    primary key (emp_no),
+    foreign key (emp_title_id) references titles (title_id)
 );
 
 --create departments table
 CREATE TABLE departments (
     dept_no varchar not null,
     dept_name varchar not null,
-	primary key (dept_no)
+    primary key (dept_no)
 );
 
 --create department managers table
 CREATE TABLE dept_manager (
     dept_no varchar not null,
     emp_no int not null,
-	foreign key (emp_no) references employees (emp_no),
-	foreign key (dept_no) references departments (dept_no),	
-	primary key (dept_no, emp_no)
+    foreign key (emp_no) references employees (emp_no),
+    foreign key (dept_no) references departments (dept_no),	
+    primary key (dept_no, emp_no)
 );
 
 --create department employees table
 CREATE TABLE dept_emp (
     emp_no int not null,
     dept_no varchar not null,
-	foreign key (emp_no) references employees(emp_no)
+    foreign key (emp_no) references employees(emp_no)
 );
 
 --create salaries table
 CREATE TABLE salaries (
     emp_no int not null,
     salary int not null,
-	primary key (emp_no),
-	foreign key (emp_no) references employees (emp_no)
+    primary key (emp_no),
+    foreign key (emp_no) references employees (emp_no)
 ); 
